01eb05558e62fa670c1d873b33e38a88b31c2485
Pablo Acosta <sonlosdias@gmail.com>
2022-03-29 15:54:57 -0300

    Added more newspapers and better source description

--- index.js
+++ index.js
@@ -8,17 +8,68 @@ const newspapers = [
     {
         name: 'times',
         address: 'https://www.thetimes.co.uk/environment/climate-change',
-        base: ''
+        base: '',
+        source: 'The Times (UK)'
     },
     {
         name: 'guardian',
         address: 'https://www.theguardian.com/environment/climate-crisis',
-        base: ''
+        base: '',
+        source: 'The Guardian (UK)'
     },
     {
         name: 'telegraph',
         address: 'https://www.telegraph.co.uk/climate-change',
-        base: 'https://www.telegraph.co.uk'
+        base: 'https://www.telegraph.co.uk',
+        source: 'Telegraph (UK)'
+    },
+    {
+        name: 'nytimes',
+        address: 'https://www.nytimes.com/section/climate',
+        base: 'https://www.nytimes.com',
+        source: 'The New York Times (USA)'
+    },
+    {
+        name: 'wsournal',
+        address: 'https://www.wsj.com/news/types/environment-science',
+        base:'',
+        source: 'The Wall Street Journal (USA)'
+    },
+    {
+        name: 'washingtonpost',
+        address: 'https://www.washingtonpost.com/climate-environment/',
+        base: '',
+        source: 'The Washington Post (USA)'
+    },
+    {
+        name: 'independent',
+        address: 'https://www.independent.co.uk/climate-change/news',
+        base: 'https://www.independent.co.uk',
+        source: 'The Independent (UK)'
+    },
+    {
+        name: 'timesofindia',
+        address: 'https://timesofindia.indiatimes.com/home/environment',
+        base:'',
+        source: 'The Times of India' 
+    },
+    {
+        name: 'sydneymorningherald',
+        address: 'https://www.smh.com.au/environment/climate-change',
+        base:'',
+        source: 'The Sidney Morning Herald (Australia)'
+    },
+    {
+        name: 'dailymail',
+        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+        base:'',
+        source: 'Daily Mail (UK)'
+    },
+    {
+        name: 'batimes',
+        address: 'https://www.batimes.com.ar/topics/climate-change',
+        base: '',
+        source: 'Buenos Aires Times (Argentina)'
     }
 ]
 
@@ -36,7 +87,7 @@ newspapers.forEach(newspaper => {
                 articles.push({
                     title,
                     url: newspaper.base + url,
-                    source: newspaper.name
+                    source: newspaper.source
                 })
             })
         })
@@ -54,6 +105,7 @@ app.get('/news/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
     const newspaperAddress = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].address
     const newspaperBase = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].base
+    const newspaperSource = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].source
 
     axios.get(newspaperAddress)
         .then((response) => {
@@ -67,7 +119,7 @@ app.get('/news/:newspaperId', (req, res) => {
                 specificArticles.push({
                     title,
                     url: newspaperBase + url,
-                    source: newspaperId
+                    source: newspaperSource
                 })
             })
             res.json(specificArticles)
